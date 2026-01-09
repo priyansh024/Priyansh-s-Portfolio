f55a3ce83b40f5c24b16e5fabbfe4fa919ae3a57
priyansh024 <priyanshmewada24@gmail.com>
2026-01-09 15:49:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // For GitHub Pages: replace 'priyansh-portfolio' with your repo name
-  base: mode === "production" ? "/priyansh-portfolio/" : "/",
+base: "/",
   server: {
     host: "::",
     port: 8080,
